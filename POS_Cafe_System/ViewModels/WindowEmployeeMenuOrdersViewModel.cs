87df46d6a15d587c1f07eace3d74d08d0729c6d3
Slavan-ST <slavan990900000@gmail.com>
2023-09-30 23:55:01 +0700

    макет создан

---
+++ POS_Cafe_System/ViewModels/WindowEmployeeMenuOrdersViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ReactiveUI;
+using ReactiveUI.Fody;
+
+namespace POS_Cafe_System.ViewModels
+{
+    public class WindowEmployeeMenuOrdersViewModel:ReactiveObject
+    {
+
+    }
+}
---
+++ POS_Cafe_System/ViewModels/WindowReadinessOrderViewModel.cs
@@ -0,0 +1,15 @@
+﻿using ReactiveUI;
+using ReactiveUI.Fody;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POS_Cafe_System.ViewModels
+{
+    public class WindowReadinessOrderViewModel:ReactiveObject
+    {
+
+    }
+}
