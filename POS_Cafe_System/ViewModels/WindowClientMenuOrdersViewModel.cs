d8719f34aa2e03f6547abb7d174b073418ce56a9
Slavan-ST <slavan990900000@gmail.com>
2023-10-01 01:26:23 +0700

    1.10.2023  01.26

--- POS_Cafe_System/ViewModels/WindowClientMenuOrdersViewModel.cs
+++ POS_Cafe_System/ViewModels/WindowClientMenuOrdersViewModel.cs
@@ -26,15 +26,19 @@ namespace POS_Cafe_System.ViewModels
             //у клиента есть корзина(заказ) куда он будет добавлять предметы, этот метод будет у всех предметов в списке
             AddInOrder = ReactiveCommand.Create(() =>
             {
-
+                OrderItems.OrderItems.Add((Items[SelectedItem], 1));
             });
             ReduceCount = ReactiveCommand.Create(() =>
             {
-
+                OrderItems.OrderItems[SelectedOrderItems] = (OrderItems.OrderItems[SelectedOrderItems].item, OrderItems.OrderItems[SelectedOrderItems].count - 1); //уменьшение количества выбранного товара в корзине
+                if (OrderItems.OrderItems[SelectedOrderItems].count == 0)
+                {
+                    OrderItems.OrderItems.Remove(OrderItems.OrderItems[SelectedOrderItems]);
+                }
             });
             AddCount= ReactiveCommand.Create(() =>
             {
-
+                OrderItems.OrderItems[SelectedOrderItems] = (OrderItems.OrderItems[SelectedOrderItems].item, OrderItems.OrderItems[SelectedOrderItems].count + 1);//увеличение количества выбранного товара в корзине
             });
         }
         public ReactiveCommand<Unit, Unit> CreateOrder { get; }
@@ -42,8 +46,13 @@ namespace POS_Cafe_System.ViewModels
         public ReactiveCommand<Unit, Unit> AddCount { get; }
         public ReactiveCommand<Unit, Unit> ReduceCount { get; }
         [Reactive]
-        public List<Order> OrderItems { get; set; } = new List<Order>();// то что будет в корзине
+        public Order OrderItems { get; set; } = new Order(new List<(ItemOrder item, int count)>()); // то что будет в корзине
         [Reactive]
         public List<ItemOrder> Items { get; set; }// то что может заказать клиент
+
+        [Reactive]
+        public int SelectedItem { get; set; }// то что может заказать клиент, выбранный предмет
+        [Reactive]
+        public int SelectedOrderItems { get; set; }// то что будет в корзине, выбранный предмет
     }
 }
